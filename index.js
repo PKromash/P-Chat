975fb448a50a6917d4618daf346f4ea66358294a
Peyton Kromash <pkromash@gmail.com>
2023-05-28 19:53:46 -0400

    m

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
+console.log(process.env.CONNECTION_URL);
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
