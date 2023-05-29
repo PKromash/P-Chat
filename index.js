e6f96b86b943970af2b83765c277b7720621b136
Peyton Kromash <pkromash@gmail.com>
2023-05-28 21:52:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://p-chat-kohl.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+const corsOptions = {
+  origin: "https://p-chat-kohl.vercel.app",
+  methods: "GET, POST, OPTIONS",
+  allowedHeaders: "Content-Type",
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
