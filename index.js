667f7e127119f04c7c0c79abd698c476e768f815
Peyton Kromash <pkromash@gmail.com>
2023-05-28 21:44:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://p-chat-kohl.vercel.app",
+  })
+);
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 
@@ -16,7 +20,6 @@ app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
-console.log(process.env.CONNECTION_URL);
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
