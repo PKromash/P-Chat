18092c733fc7798a93a2ba43935826a83515b1b2
Peyton Kromash <pkromash@gmail.com>
2023-05-29 10:22:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/users.js";
 import dotenv from "dotenv";
 
 const app = express();
-dotenv.config();
+dotenv.config(); 
 
 const corsOptions = {
   origin: "https://p-chat-kohl.vercel.app",
