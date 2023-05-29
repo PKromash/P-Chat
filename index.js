78781b847e7b5bfe2e4142716b7297b9108b6b13
Peyton Kromash <pkromash@gmail.com>
2023-05-29 09:51:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
   allowedHeaders: "Content-Type, Authorization",
 };
 
-app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
